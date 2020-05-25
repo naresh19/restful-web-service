f5861bfffe651fa49da2dd70aa9d76d410d74fb9
naresh <naresh.sherwal@splashmath.com>
2020-05-25 19:39:43 +0530

    h2 jpa added

--- src/main/java/com/naresh/firstservice/restfulwebservice/user/User.java
+++ src/main/java/com/naresh/firstservice/restfulwebservice/user/User.java
@@ -4,27 +4,40 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.BatchSize;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
-//@Entity
+
 //@JsonIgnoreProperties(value = {"dob", "id"})
+@Entity
 public class User {
-//    @Id
 //    @Column(name="id")
+    @Id
+    @GeneratedValue
     private int id;
 
     @NotNull(message = "Name cannot be null")
     @Size(min=2, message="Name should have atleast 2 characters")
     private String name;
 
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private List<Post> posts;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+
     @Past
-    @JsonIgnore
+//    @JsonIgnore
     private Date dob;
     private static int counter = 3;
 
